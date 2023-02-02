64bdba9994864da08b18facd90280b8117b39eff
John Maximilian <2e0byo@gmail.com>
2023-02-02 11:13:21 +0000

    feat: lisp glyphs.

--- rust_src/crates/emacs/src/glyph.rs
+++ rust_src/crates/emacs/src/glyph.rs
@@ -1,9 +1,19 @@
 use std::{cmp::min, slice};
 
 use crate::{
-    bindings::{composition_hash_table, composition_method, glyph, glyph_string, XHASH_TABLE},
+    bindings::{
+        composition_hash_table,
+        composition_method,
+        glyph,
+        glyph_string,
+        XHASH_TABLE,
+        composition_gstring_from_id,
+        AREF,
+        lglyph_indices::{LGLYPH_IX_ADJUSTMENT,
+                         LGLYPH_IX_WIDTH},
+        VECTORP},
     definitions::EmacsInt,
-    lisp::ExternalPtr,
+    lisp::{ExternalPtr, LispObject},
 };
 
 pub type XChar2b = u32;
@@ -53,7 +63,7 @@ impl GlyphStringRef {
         let composition = composition.as_vector().unwrap();
 
         let glyph_index = if unsafe { (*self.cmp).method }
-            == composition_method::COMPOSITION_WITH_RULE_ALTCHARS
+        == composition_method::COMPOSITION_WITH_RULE_ALTCHARS
         {
             n * 2
         } else {
@@ -64,6 +74,8 @@ impl GlyphStringRef {
 
         glyph.as_fixnum_or_error()
     }
+
+
 }
 
 impl IntoIterator for GlyphStringRef {
@@ -97,5 +109,86 @@ impl Iterator for GlyphStringIntoIterator {
         self.next_glyph_string = new_next;
 
         result
+    }
+}
+
+// Lisp Glyph Strings are the other side of the pipeline.  See `composite.h`.
+
+// pub type LGString = LispObject;
+
+/// A Lisp Glyph (LGLYPH).
+struct LGlyph {
+    ptr: LispObject
+}
+
+
+impl LGlyph {
+    ///
+    pub fn new(ptr: LispObject) -> LGlyph { LGlyph { ptr } }
+
+    pub fn is_nil(&self) -> bool { self.ptr.is_nil() }
+
+    fn adjustment_ptr(&self) -> LispObject {
+        unsafe { AREF(self.ptr, LGLYPH_IX_ADJUSTMENT as isize)}
+    }
+
+    fn adjustment_value(&self, i: isize) -> EmacsInt {
+        let adj = self.adjustment_ptr();
+        if unsafe { VECTORP(adj) } {
+            (unsafe {AREF(adj, i) }).as_fixnum_or_error()
+        } else {
+            0 as EmacsInt
+        }
+    }
+
+    pub fn width(&self) -> EmacsInt {
+        (unsafe { AREF(self.ptr, LGLYPH_IX_WIDTH as isize)}).as_fixnum_or_error()
+    }
+
+    pub fn has_adjustment(&self) -> bool { self.adjustment_ptr().is_nil() }
+
+    fn x_offset(&self) -> EmacsInt {self.adjustment_value( 0)}
+
+    fn y_offset(&self) -> EmacsInt {self.adjustment_value( 1)}
+
+    fn width_adjustment(&self) -> EmacsInt {self.adjustment_value( 2)}
+}
+
+struct LGString {
+    ptr: LispObject,
+    idx: isize,
+
+}
+
+impl LGString {
+    pub fn new(ptr: LispObject) -> LGString {
+        LGString {
+            ptr,
+            idx: 0,
+        }
+    }
+
+    pub fn from_id(id: isize) -> LGString {
+        let ptr = unsafe { composition_gstring_from_id(id) };
+        Self::new(ptr)
+    }
+
+    pub fn glyph(&self, idx: isize) -> LGlyph {
+        LGlyph::new( unsafe { AREF(self.ptr, idx + 2) } )
+    }
+}
+
+impl Iterator for LGString {
+    type Item = LGlyph;
+
+    // We don't keep a reference to the glyph.  Should we?
+    fn next(&mut self) -> Option<Self::Item> {
+        self.idx += 1;
+        let next_glyph = self.glyph(self.idx);
+        if next_glyph.is_nil() {
+            None
+        } else {
+            Some(next_glyph)
+        }
     }
 }
